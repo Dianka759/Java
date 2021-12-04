f0693c7487e58487da79968436f60763bf90c287
dianka759 <dianka759@gmail.com>
2021-12-03 22:51:12 -0500

    first commit

--- SpringProjects/fruityloops/src/main/java/com/codingdojo/fruityloops/controllers/ItemController.java
+++ SpringProjects/fruityloops/src/main/java/com/codingdojo/fruityloops/controllers/ItemController.java
@@ -15,9 +15,9 @@ public class ItemController {
  public String index(Model model) {
      
      ArrayList<Item> fruits = new ArrayList<Item>();
-     fruits.add(new Item("Kiwi", 1.50));
-     fruits.add(new Item("Mango", 2.00));
-     fruits.add(new Item("Goji Berries", 4.00));
+     fruits.add(new Item("Kiwi", 1.5));
+     fruits.add(new Item("Mango", 2.0));
+     fruits.add(new Item("Goji Berries", 4.0));
      fruits.add(new Item("Guava", .75));
      
      // Add fruits your view model here
